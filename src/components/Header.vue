8e0767710841fe4687f6099a1efb2a2544447d17
Mateusz Hołowieszko <holowieszko.m@gmail.com>
2019-10-22 15:40:38 +0200

    Header from App.vue now has own component

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app-bar app class="header">
+    <v-toolbar-title class="headline text-uppercase">
+      <router-link to="/" id="title">
+        <span class="font-weight-bold">Openchaching</span>
+        <span class="font-weight-light">logger</span>
+      </router-link>
+    </v-toolbar-title>
+    <v-spacer></v-spacer>
+     <v-toolbar-items>
+        <v-btn text to="/login">Login</v-btn>
+      </v-toolbar-items>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.header #title {
+  color: #000;
+  text-decoration: none;
+}
+</style>
