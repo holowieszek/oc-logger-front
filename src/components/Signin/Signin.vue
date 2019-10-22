f612de184c3cf76f79b3c83cdd4c609447e4a926
Mateusz Hołowieszko <holowieszko.m@gmail.com>
2019-10-22 16:46:45 +0200

    Call oauth/requestToken

--- src/components/Signin/Signin.vue
+++ src/components/Signin/Signin.vue
@@ -1,15 +1,43 @@
 <template>
-  <h1>login</h1>
+  <h1>Zaloguj się</h1>
 </template>
 
 <script>
+import asyncWrapper from '../../utils/asyncWrapper';
 export default {
   data: () => ({
 
   }),
-  created() {
+  
+  methods: {
+    async getOAuthToken() {
+      const { error, result } = await asyncWrapper(this.$api.get('oauth/requestToken'));
 
-  }
+      if (result) {
+        const params = this.getParams(result.data, ['oauth_token', 'oauth_token_secret']);
+
+        localStorage.setItem('oauth_token', params.oauth_token);
+        localStorage.setItem('oauth_token_secret', params.oauth_token_secret);
+
+        window.location.href = `https://opencaching.pl/okapi/services/oauth/authorize?oauth_token=${params.oauth_token}`; 
+      } else {
+        console.error(error);
+      }
+    },
+    getParams(query, find) {
+      let params = new URLSearchParams(query);
+      const data = [];
+
+      find.map(key => {
+        return data[key] = params.get(key)
+      });
+
+      return data;
+    }
+  },
+  created() {
+    this.getOAuthToken();
+  },
 }
 </script>
 
