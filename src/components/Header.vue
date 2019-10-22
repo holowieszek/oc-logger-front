681c75e1767bc340c62ffd9ae8bd38b95fb03a40
Mateusz Hołowieszko <holowieszko.m@gmail.com>
2019-10-22 16:46:25 +0200

    Fixed typo in header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
   <v-app-bar app class="header">
     <v-toolbar-title class="headline text-uppercase">
       <router-link to="/" id="title">
-        <span class="font-weight-bold">Openchaching</span>
+        <span class="font-weight-bold">Opencaching</span>
         <span class="font-weight-light">logger</span>
       </router-link>
     </v-toolbar-title>
