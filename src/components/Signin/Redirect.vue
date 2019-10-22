1a705ea92cc0584e18098e4287dddcc3d4e48a1e
Mateusz Hołowieszko <holowieszko.m@gmail.com>
2019-10-22 16:47:20 +0200

    Get oauth_verifier after redirect then call oauth/accessToken to make transaction but it's not working at the moment

--- src/components/Signin/Redirect.vue
+++ src/components/Signin/Redirect.vue
@@ -3,10 +3,35 @@
 </template>
 
 <script>
+import asyncWrapper from '../../utils/asyncWrapper';
+
 export default {
   data: () => ({
 
   }),
+  methods: {
+    async getAccessToken() {
+      const oauthVerifier = new URLSearchParams(window.location.search).get('oauth_verifier')
+      const oauthToken = localStorage.getItem('oauth_token');
+      const oauthTokenSecret = localStorage.getItem('oauth_token_secret');
+
+      
+      if (oauthVerifier && oauthToken && oauthTokenSecret) {
+        const data = {
+          oauthVerifier,
+          oauthToken,
+          oauthTokenSecret
+        }
+
+        const { error, result } = await asyncWrapper(this.$api.post('oauth/accessToken', data));
+
+        console.log(error, result);
+      }
+    }
+  },
+  created() {
+    this.getAccessToken();
+  }
 }
 </script>
 
