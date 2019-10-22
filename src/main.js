5859fa21f6fc2ec002f91350a335e56d1d1ccc5e
Mateusz Hołowieszko <holowieszko.m@gmail.com>
2019-10-22 16:47:30 +0200

    Added axios plugin

--- src/main.js
+++ src/main.js
@@ -2,7 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'
+import axios from 'axios'
+
+Vue.use({
+  install (Vue) {
+    Vue.prototype.$api = axios.create({
+      baseURL: 'https://7y57t17d3a.execute-api.eu-central-1.amazonaws.com/dev/'
+    })
+  }
+})
 
 Vue.config.productionTip = false
 
