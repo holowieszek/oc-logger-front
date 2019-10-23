3a7f6f286b8c4af01ff3c53f309ec2962d85afaf
Mateusz Hołowieszko <holowieszko.m@gmail.com>
2019-10-23 23:24:59 +0200

    Don't display site until you'll get response from /oc/user endpoint

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <v-app>
-    <Header />
-    <v-content>
-      <router-view></router-view>
-    </v-content>
+    <div v-if="!isLoading">
+      <Header />
+      <v-content>
+        <router-view></router-view>
+      </v-content>
+    </div>
+    <p v-else>Loading</p>
   </v-app>
 </template>
 
 <script>
 import Header from './components/Header';
-import { mapActions } from 'vuex';
+import { mapActions, mapGetters } from 'vuex';
 
 export default {
   name: 'App',
@@ -17,6 +20,7 @@ export default {
     //
   }),
   components: { Header },
+  computed: mapGetters(['isLoading']),
   methods: mapActions(['checkIfAuthenticated']),
   created() {
     // const oauthToken = localStorage.getItem('oauth_token');
