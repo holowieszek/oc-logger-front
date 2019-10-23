e992000aa5670837f9bce0f955186dc8c7a8098a
Mateusz Hołowieszko <holowieszko.m@gmail.com>
2019-10-23 13:29:40 +0200

    Removed axios plugin, added baseURL globally (not best practice I guess

--- src/main.js
+++ src/main.js
@@ -5,13 +5,23 @@ import store from './store'
 import vuetify from './plugins/vuetify'
 import axios from 'axios'
 
-Vue.use({
-  install (Vue) {
-    Vue.prototype.$api = axios.create({
-      baseURL: 'https://7y57t17d3a.execute-api.eu-central-1.amazonaws.com/dev/'
-    })
-  }
-})
+
+
+axios.defaults.baseURL = 'https://7y57t17d3a.execute-api.eu-central-1.amazonaws.com/dev/'
+
+
+// const instance = axios.create({
+//   baseURL: 'https://7y57t17d3a.execute-api.eu-central-1.amazonaws.com/dev/'
+// })
+
+// Vue.prototype.$api = instance;
+// Vue.use({
+//   install (Vue) {
+//     Vue.prototype.$api = axios.create({
+//       baseURL: 'https://7y57t17d3a.execute-api.eu-central-1.amazonaws.com/dev/'
+//     })
+//   }
+// })
 
 Vue.config.productionTip = false
 
