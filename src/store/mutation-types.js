7d8ad928ac9fba7f375ed5dc95585b73b391c2a0
Mateusz Hołowieszko <holowieszko.m@gmail.com>
2019-10-24 22:59:56 +0200

    Added mutation_type for handling geocaches

--- src/store/mutation-types.js
+++ src/store/mutation-types.js
@@ -1,3 +1,5 @@
 export const SET_IS_LOGGED_IN = 'SET_IS_LOGGED_IN';
 export const SET_USERNAME = 'SET_USERNAME';
 export const SET_IS_LOADING = 'SET_IS_LOADING';
+
+export const SET_GEOCACHES = 'SET_GEOCACHES';
