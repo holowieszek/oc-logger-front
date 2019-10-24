6ea7ffcec295b9e537582a6050446cf98ae41097
Mateusz Hołowieszko <holowieszko.m@gmail.com>
2019-10-24 19:10:32 +0200

    Dashboard component moved to another dir

---
+++ src/components/Dashboard/Dashboard.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-container fluid class="dashboard">
+    <!-- <h1 class="text-center">{{ username }}</h1> -->
+    <v-row>
+      <v-col lg="3" class="left">
+        <p>left</p>
+      </v-col>
+      <v-col lg="9" class="right">
+        <p>right</p>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import asyncWrapper from '../../utils/asyncWrapper';
+import { mapGetters } from 'vuex';
+
+export default {
+  data: () => ({
+
+  }),
+  computed: mapGetters(['username'])
+}
+</script>
+
+<style scoped>
+.dashboard {
+  background: red !important;
+}
+
+.dashboard .left {
+  background: blue;
+}
+
+.dashboard .right {
+  background: yellow;
+}
+</style>
