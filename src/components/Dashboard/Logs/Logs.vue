50e6e031134f96fed89b3287717fcba7a224844e
Mateusz Hołowieszko <holowieszko.m@gmail.com>
2019-10-25 17:47:28 +0200

    Display geocaches from locus file

--- src/components/Dashboard/Logs/Logs.vue
+++ src/components/Dashboard/Logs/Logs.vue
@@ -4,16 +4,25 @@
     tile
     class="logs"
   >
-    <div v-for="n in 15" :key="n">
+    <!-- <div v-for="n in 15" :key="n">
       <LogItem :cacheName="'cache name'" />
+    </div> -->
+
+    <div v-if="geocaches">
+      <div v-for="(geocache, index) in geocaches" :key="index">
+        <LogItem :cacheName="geocache.name" />
+      </div>
     </div>
   </v-card>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
 import LogItem from './LogItem';
+
 export default {
   data: () => ({}),
+  computed: mapGetters(['geocaches']),
   components: { LogItem }
 }
 </script>
